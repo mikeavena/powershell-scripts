fe4c300fe23d8fa72af2884b2e77f2cf0e0c549d
Michael Avena <michael.avena@gmail.com>
2023-04-12 20:03:20 -0400

    Added scripts copy and integrity check user folder

---
+++ Copy-UserFolder.ps1
@@ -0,0 +1,15 @@
+# Author: Mike Avena
+# License: MIT
+# Purpose: Copies a specified user folder to a target location using Robocopy. Preserves file timestamps. Excludes AppData and Application Data folders.
+# Output: Log file with errors.
+
+Param
+(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string] $User,
+    [Parameter(Mandatory=$true, Position=1)]
+    [ValidateScript({ Test-Path $_ })]
+    [string] $Target
+)
+
+Robocopy C:\Users\$User $Target /XJ /XD C:\Users\$User\AppData /DCOPY:T /COPY:DAT /E /R:0 /LOG:C:\CopyUserFolder.LOG /nfl /ndl /njh /njs /ns /nc /np /tee
